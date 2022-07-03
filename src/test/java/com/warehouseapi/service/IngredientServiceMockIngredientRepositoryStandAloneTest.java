7922d8f3c510751fcaab7085ed43b11a43fefa6a
EddeCCC <eduard.schander@gmx.de>
2022-07-03 19:17:09 +0200

    fix service tests with dto

--- src/test/java/com/warehouseapi/service/IngredientServiceMockIngredientRepositoryStandAloneTest.java
+++ src/test/java/com/warehouseapi/service/IngredientServiceMockIngredientRepositoryStandAloneTest.java
@@ -1,13 +1,11 @@
 package com.warehouseapi.service;
 
-import org.junit.jupiter.api.Assertions;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.junit.jupiter.api.*;
+import warehouse.api.dto.IngredientDTO;
 import warehouse.api.repository.IngredientRepository;
 import warehouse.api.service.DTOMapper;
 import warehouse.api.service.IngredientService;
 import org.junit.Assert;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -15,27 +13,26 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import warehouse.api.entity.Ingredient;
 import warehouse.api.exception.IngredientNotFoundException;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 import static org.mockito.Mockito.when;
 
 @ExtendWith(SpringExtension.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class IngredientServiceMockIngredientRepositoryStandAloneTest {
 
 	@Mock
 	private IngredientRepository mockedIngredientRepository;
-	@MockBean
+	@Mock
 	private DTOMapper dtoMapper;
 
 	@InjectMocks
 	private IngredientService ingredientService;
+
 	private Ingredient ingredient;
 
 
-	@BeforeEach
+	@BeforeAll
 	public void init() {
 		this.ingredient = new Ingredient(20L, "Salami", "jaa", "italy",
 				'd', 350, 1, 100.0, 4.0);
@@ -43,13 +40,16 @@ class IngredientServiceMockIngredientRepositoryStandAloneTest {
 
 	@Test
 	void getIngredientById_GoodCase() throws IngredientNotFoundException {
-		when(mockedIngredientRepository.findById(20L)).thenReturn(Optional.ofNullable((this.ingredient)));
-		//int i = ingredientService.getIngredient(20L).getCalories();
+		IngredientDTO dto = new IngredientDTO(20L, "Salami", "jaa", "italy",
+				'd', 350, 1, 100.0, 4.0);
+
+		when(mockedIngredientRepository.findById(20L)).thenReturn(Optional.of(this.ingredient));
+		when(dtoMapper.toIngredientDTO(this.ingredient)).thenReturn(dto);
 
 		Assert.assertEquals("Salami", ingredientService.getIngredient(20L).getName());
 		Assert.assertEquals("italy", ingredientService.getIngredient(20L).getCountryOrigin());
 		Assert.assertEquals('d', ingredientService.getIngredient(20L).getNutritionScore());
-		Assert.assertEquals(350, (int)(ingredientService.getIngredient(20L).getCalories()));
+		Assert.assertEquals(350, (int)ingredientService.getIngredient(20L).getCalories());
 		Assert.assertEquals(1, (int)ingredientService.getIngredient(20L).getAmount());
 		Assertions.assertEquals(100.0, (double)ingredientService.getIngredient(20L).getWeight());
 		Assertions.assertEquals(4.0, (double)ingredientService.getIngredient(20L).getPrice());
@@ -57,27 +57,38 @@ class IngredientServiceMockIngredientRepositoryStandAloneTest {
 	@Test
 	void getIngredientById_NotFoundException() throws IngredientNotFoundException {
 		when(mockedIngredientRepository.findById(20L)).thenReturn(Optional.ofNullable((null)));
+
 		IngredientNotFoundException thrown = Assert.assertThrows(IngredientNotFoundException.class, () -> {
 			ingredientService.getIngredient(20L);
 		});
-		Assert.assertEquals("Ingredient with id :20 not found in Database", thrown.getMessage());
+		Assert.assertEquals("Ingredient with id: 20 not found in Database", thrown.getMessage());
 	}
 	@Test
 	void getIngredients_ShouldReturnIngredientList() throws IngredientNotFoundException {
-		Ingredient ingredient1 = new Ingredient(30L, "Brot", "noname", "spain", 'b', 200, 1, 100.0, 1.0);
-		List<Ingredient> ingredientList = new ArrayList(Arrays.asList(ingredient1, this.ingredient));
+		Ingredient ingredient1 = new Ingredient(30L, "Brot", "noname", "spain", 'b',
+				200, 1, 100.0, 1.0);
+		IngredientDTO dto = new IngredientDTO(20L, "Salami", "jaa", "italy",
+				'd', 350, 1, 100.0, 4.0);
+		IngredientDTO dto1 = new IngredientDTO(30L, "Brot", "noname", "spain", 'b',
+				200, 1, 100.0, 1.0);
+
+		List<Ingredient> ingredientList = List.of(ingredient1, this.ingredient);
 		when(mockedIngredientRepository.findAll()).thenReturn(ingredientList);
-		Assert.assertEquals("Brot",ingredientService.getIngredients().get(0).getName());
-		Assert.assertEquals("noname",ingredientService.getIngredients().get(0).getBrand());
-		Assert.assertEquals("spain",ingredientService.getIngredients().get(0).getCountryOrigin());
-		Assert.assertEquals('b',ingredientService.getIngredients().get(0).getNutritionScore());
-		Assert.assertEquals(200L,(int)ingredientService.getIngredients().get(0).getCalories());
+		when(dtoMapper.toIngredientDTO(this.ingredient)).thenReturn(dto);
+		when(dtoMapper.toIngredientDTO(ingredient1)).thenReturn(dto1);
+
+		Assert.assertEquals("Brot", ingredientService.getIngredients().get(0).getName());
+		Assert.assertEquals("noname", ingredientService.getIngredients().get(0).getBrand());
+		Assert.assertEquals("spain", ingredientService.getIngredients().get(0).getCountryOrigin());
+		Assert.assertEquals('b', ingredientService.getIngredients().get(0).getNutritionScore());
+		Assert.assertEquals(200, (int)ingredientService.getIngredients().get(0).getCalories());
 		Assert.assertEquals("Salami", ingredientService.getIngredients().get(1).getName());
 	}
 	@Test
 	void getIngredients_ShouldReturnEmptyIngredientList() throws IngredientNotFoundException {
-		List<Ingredient>emptyIngredientList=new ArrayList<>();
+		List<Ingredient> emptyIngredientList = new LinkedList<>();
 		when(mockedIngredientRepository.findAll()).thenReturn(emptyIngredientList);
+
 		Assert.assertEquals(0,ingredientService.getIngredients().size());
 	}
 }
